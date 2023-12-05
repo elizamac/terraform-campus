6fc2c07612c2fbeb5a00dabd8436fce14780a395
elizamac <elizaf.macovei@gmail.com>
2023-12-05 23:19:10 +0000

    Changed missleading variable names

--- leclerc/twingate.tf
+++ leclerc/twingate.tf
@@ -2,29 +2,24 @@ resource "docker_image" "twingate" {
   name = "twingate/connector:1"
 }
 
-data "tfe_workspace" "leclerc" {
-  name = "leclerc"
-  organization = "ElizaMacovei"
-}
-
-variable "tfc_access_token" {
+variable "twingate_access_token" {
   type      = string
   sensitive = true
   default = ""
 }
 
-variable "tfc_refresh_token" {
+variable "twingate_refresh_token" {
   type      = string
   sensitive = true
   default = ""
 }
 
-resource "docker_container" "tfc-connector" {
-  name     = "tfc-connector"
+resource "docker_container" "twingate-connector" {
+  name     = "twingate-connector"
   image    = docker_image.twingate.image_id
-  hostname = "tfc-connector"
+  hostname = "twingate-connector"
 
   network_mode = "host"
   restart      = "always"
-  env          = ["TWINGATE_NETWORK=elizamac", "TWINGATE_LABEL_HOSTNAME=`hostname`", "TWINGATE_ACCESS_TOKEN=${var.tfc_access_token}", "TWINGATE_REFRESH_TOKEN=${var.tfc_refresh_token}"]
+  env          = ["TWINGATE_NETWORK=elizamac", "TWINGATE_LABEL_HOSTNAME=`hostname`", "TWINGATE_ACCESS_TOKEN=${var.twingate_access_token}", "TWINGATE_REFRESH_TOKEN=${var.twingate_refresh_token}"]
 }
