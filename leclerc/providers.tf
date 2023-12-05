7f07d17a0acd12e5daf7c540f19bfdd22c3ab0ac
elizamac <elizaf.macovei@gmail.com>
2023-12-05 23:08:53 +0000

    tried to fix variables

--- leclerc/providers.tf
+++ leclerc/providers.tf
@@ -4,9 +4,17 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "3.0.2"
     }
+    /*
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.50.0"
+    }
+    */
   }
 }
 
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
+
+//provider "tfe" {}
