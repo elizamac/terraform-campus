4805af7224587029aa1cf63a0ae96bd544284e7b
elizamac <elizaf.macovei@gmail.com>
2023-12-09 01:49:07 +0000

    Formatting

--- fia/perez.tf
+++ fia/perez.tf
@@ -6,27 +6,27 @@ variable "fia_node_name" {
 resource "proxmox_lxc" "perez" {
   target_node = var.fia_node_name
   hostname    = "perez"
-  vmid = 202
+  vmid        = 202
   clone       = 921
-  full = true
+  full        = true
 
   onboot = true
-  start = true
-  
+  start  = true
+
   mountpoint {
-    key = "1"
-    slot = 1
+    key     = "1"
+    slot    = 1
     storage = "/mnt/data"
-    volume = "/mnt/data"
-    mp = "/mnt/data"
+    volume  = "/mnt/data"
+    mp      = "/mnt/data"
   }
 
   network {
-    name = "eth0"
-    bridge = "vmbr1"
+    name     = "eth0"
+    bridge   = "vmbr1"
     firewall = false
-    ip = "10.94.117.21/24"
-    gw = "10.94.117.1"
-    ip6 = "auto"
+    ip       = "10.94.117.21/24"
+    gw       = "10.94.117.1"
+    ip6      = "auto"
   }
 }
