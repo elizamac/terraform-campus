fcf4a6ea95e097f98d24fdb6241d4964ddbc6f67
elizamac <elizaf.macovei@gmail.com>
2023-12-10 12:04:28 +0000

    fixed gateway address

--- leclerc/network_pterodactyl-backend.tf
+++ leclerc/network_pterodactyl-backend.tf
@@ -5,7 +5,7 @@ resource "docker_network" "pt-backend" {
   driver = "bridge"
 
   ipam_config {
-    gateway = "172.16.45.0/24"
+    gateway = "172.16.45.1/24"
     subnet = "172.16.45.0/24"
     ip_range = "172.16.45.0/24"
   }
