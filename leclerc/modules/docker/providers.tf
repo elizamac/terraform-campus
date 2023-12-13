4455faff34d28735025316b26fe901e8070c00f7
elizamac <elizaf.macovei@gmail.com>
2023-12-13 18:27:55 +0000

    moved to modules + docker config for Lando

--- leclerc/modules/docker/providers.tf
+++ leclerc/modules/docker/providers.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "3.0.2"
     }
-    linux = {
-      source  = "mavidser/linux"
-      version = "1.0.2"
-    }
   }
 }
 
@@ -19,9 +15,4 @@ variable "master_password" {
   type      = string
   sensitive = true
   default   = ""
-}
-provider "linux" {
-  host     = "10.94.117.21"
-  user     = "root"
-  password = var.master_password
 }
