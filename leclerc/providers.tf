13135d6628ee1b852489d96f667e4524519a0c41
elizamac <elizaf.macovei@gmail.com>
2023-12-13 17:37:31 +0000

    add remote user to Leclerc

--- leclerc/providers.tf
+++ leclerc/providers.tf
@@ -4,9 +4,24 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "3.0.2"
     }
+    linux = {
+      source = "mavidser/linux"
+      version = "1.0.2"
+    }
   }
 }
 
 provider "docker" {
   host = "unix:///var/run/docker.sock"
+}
+
+variable "master_password" {
+  type = string
+  sensitive = true
+  default = ""
+}
+provider "linux" {
+  host = "10.94.117.21"
+  user = "root"
+  password = var.master_password
 }
