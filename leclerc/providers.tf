047ec1225226ea076f3430ecd57265fda7599e18
elizamac <elizaf.macovei@gmail.com>
2024-03-04 11:10:02 +0000

    Import CF tunnel "IE Campus"

--- leclerc/providers.tf
+++ leclerc/providers.tf
@@ -4,9 +4,23 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "3.0.2"
     }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "docker" {
   host = "unix:///var/run/docker.sock"
+}
+
+variable "cf_api_token" {
+  type = string
+  sensitive = true
+  default = ""
+}
+provider "cloudflare" {
+  api_token = var.cf_api_token
 }
