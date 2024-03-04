4f82b404f906bdeeb7295b773dd6a18754312dc7
elizamac <elizaf.macovei@gmail.com>
2024-03-04 11:27:46 +0000

    used env variable instead of tf variable

--- leclerc/providers.tf
+++ leclerc/providers.tf
@@ -16,11 +16,4 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-variable "cf_api_token" {
-  type = string
-  sensitive = true
-  default = ""
-}
-provider "cloudflare" {
-  api_token = var.cf_api_token
-}
+provider "cloudflare" {}
