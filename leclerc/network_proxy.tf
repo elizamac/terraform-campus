eae300b586ec0ef56a209936dc0a04705b9fabfa
elizamac <elizaf.macovei@gmail.com>
2024-04-03 14:02:20 +0100

    changed proxy network gateway address

--- leclerc/network_proxy.tf
+++ leclerc/network_proxy.tf
@@ -5,7 +5,7 @@ resource "docker_network" "proxy" {
   driver          = "bridge"
 
   ipam_config {
-    gateway  = "172.16.38.127"
+    gateway  = "172.16.38.1"
     subnet   = "172.16.38.0/25"
     # ip range 172.16.38.1 - 172.16.38.127  
     ip_range = "172.16.38.0/25"
